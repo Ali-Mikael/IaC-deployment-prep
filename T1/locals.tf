ec606937ae21d92e377230ab020a5329a0fb53e1
Ali.G <alimikael57@gmail.com>
2025-11-10 14:35:54 +0200

    create *.tf

---
+++ T1/locals.tf
@@ -0,0 +1,9 @@
+locals {
+  # Splitting the main CIDR (/16) into /24 subnets
+  subnets = {
+    public-1  = cidrsubnet(var.main_cidr, 8, 1)
+    public-2  = cidrsubnet(var.main_cidr, 8, 2)
+    private-1 = cidrsubnet(var.main_cidr, 8, 3)
+    private-2 = cidrsubnet(var.main_cidr, 8, 4)
+  }
+}
