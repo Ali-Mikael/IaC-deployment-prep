8d0526c64390728b58518b8e4541dd7cd5b3b68a
Ali.G <alimikael57@gmail.com>
2025-11-29 18:54:26 +0200

    add comment

--- terraform/credentials.tf
+++ terraform/credentials.tf
@@ -10,3 +10,7 @@
 #   secret_id     = aws_secretsmanager_secret.secret.id
 #   secret_string = jsonencode(var.secret)
 # }
+
+# NOTE:
+#   If you delete the resource, there will be a retention period, 
+#   before you can create the same resource, using the same secret. (same name)
