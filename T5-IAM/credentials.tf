b7a84af0a3886be94a421597fd18eeeea1e618ec
Ali-Mikael <alimikael57@gmail.com>
2025-11-30 19:08:20 +0200

    Update credentials.tf

--- T5-IAM/credentials.tf
+++ T5-IAM/credentials.tf
@@ -5,7 +5,7 @@ resource "aws_secretsmanager_secret" "secret" {
   name = "dummy-user"
 }
 
-# The secret (password) 
+# The secret
 resource "aws_secretsmanager_secret_version" "v" {
   secret_id     = aws_secretsmanager_secret.secret.id
   secret_string = jsonencode(var.secret)
