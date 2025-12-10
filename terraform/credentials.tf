25fe480320ac2f9fcd47d8feee1c4918b18da4a9
Ali.G <alimikael57@gmail.com>
2025-12-10 19:36:34 +0200

    update

--- terraform/credentials.tf
+++ terraform/credentials.tf
@@ -1,16 +1,13 @@
 # Credentials
 # -----------
 
-# resource "aws_secretsmanager_secret" "secret" {
-#   name = "dummy-user"
-# }
+# Secret will be under this name
+resource "aws_secretsmanager_secret" "secret" {
+  name = "dummy-user"
+}
 
-# # The secret (password) 
-# resource "aws_secretsmanager_secret_version" "v" {
-#   secret_id     = aws_secretsmanager_secret.secret.id
-#   secret_string = jsonencode(var.secret)
-# }
-
-# NOTE:
-#   If you delete the resource, there will be a retention period, 
-#   before you can create the same resource, using the same secret. (same name)
+# The secret
+resource "aws_secretsmanager_secret_version" "v" {
+  secret_id     = aws_secretsmanager_secret.secret.id
+  secret_string = jsonencode(var.secret)
+}
